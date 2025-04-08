e92bb3d1c9dcd13ad8ce6ff30267ccf6ed159ea5
tzer135 <tzerdongchong@gmail.com>
2025-04-08 16:21:49 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,19 @@
-const mineflayer = require('mineflayer')
+const mineflayer = require('mineflayer');
 
 const bot = mineflayer.createBot({
   host: '1217272024oooooi.aternos.me',
   port: 19987,
   username: 'huan678'
-})
+});
 
 bot.on('spawn', () => {
-  console.log('✅ 机器人 huan678 已上线！')
-})
+  console.log('✅ 机器人 huan678 已上线！');
+});
 
-bot.on('chat', (username, message) => {
-  if (username === bot.username) return
-  if (message === '跳') {
-    bot.setControlState('jump', true)
-    setTimeout(() => bot.setControlState('jump', false), 500)
-  }
-})
+bot.on('error', (err) => {
+  console.log('机器人出现错误:', err);
+});
+
+bot.on('end', () => {
+  console.log('机器人已断开连接');
+});
